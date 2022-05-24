79cba16a065b450d6205d51987dbb619a2872493
evgenija <evgenijjaa@gmail.com>
2022-05-24 18:09:57 +0200

    polepšala kodo

--- qlearning.py
+++ qlearning.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 
 def switch_state(state, action):
+    """Switches state accordind to action."""
     new_state = []
     for i in range(len(state)):
         if i != action[0]:
@@ -15,17 +16,19 @@ def switch_state(state, action):
 
 class Qlearning():
 
-    def __init__(self, alpha, epsilon):
-
+    def __init__(self, alpha, gamma, epsilon):
+        """Initialize the process with parameters and empty Q-table 
+        (all states and actions at the beginning are considered to have value 0)"""
         self.alpha = alpha
         self.epsilon = epsilon
-
-        # at the beginnign all q values are zero 
+        self.gamma = gamma
         self.Qtable = dict() 
 
 
     def choose_action(self, state, epsilon_greedy=True):
-        """In a state with possible actions in that state - chooses best action"""
+        """In a state with possible actions in that state - chooses an action.
+        If epsilon_greedy is true that means with probability self.epsilon we choose a random action,
+        otherwise we choose the action with maximal reward."""
 
         game = Nim(state)
         possible_actions = game.possible_actions()
@@ -33,152 +36,112 @@ class Qlearning():
         highest_q_value = None
         best_action = None
 
+        # For epsilon_greedy method: 
+        # generate a random number between 0 and 1 - with self.epsilon probability it will be lower that self.epsilon
         r = random.random()
         if epsilon_greedy and r < self.epsilon: # epsilon greedy method
             best_action = random.choice(possible_actions)
 
-        else: # highest possible Q value
-            
+        else: 
+            # highest possible Q value - read from the table
             for a in possible_actions:
                 if (tuple(state), a) in self.Qtable:
                     a_value = self.Qtable[(tuple(state), a)]
-                else: # if it's not in the dictionary
+                else: 
+                    # if it's not in the dictionary it has starting value 0
                     a_value = 0
-
                 if highest_q_value == None or a_value > highest_q_value:
                     highest_q_value = a_value
                     best_action = a
-                
-            # print((highest_q_value, best_action))
         return best_action
 
-
-    def max_reward(self, state):
-        game = Nim(state)
-        possible_actions = game.possible_actions()
-
-        if possible_actions == []:
-            return 0
-            # raise Exception("Winner already determined")
-        else:
-            reward = None
-            for a in possible_actions:
-                if (tuple(state), a) in self.Qtable:
-                    a_value = self.Qtable[(tuple(state), a)]
-                else:
-                    a_value = 0
-                if  reward == None or a_value > reward:
-                    reward = a_value
-
-        return reward
-
-
     def update_model(self, old_state, action, reward):
-        """Old state, action made and reward recieved from that action used to update Q learning model"""
-
+        """Old state, action made and reward recieved from that action used to update Q learning model according to the bellman equation"""
         if (tuple(old_state), action) in self.Qtable:
             old_q = self.Qtable[(tuple(old_state), action)] # q value for old state and action made
         else:
             old_q = 0
-        
-        new_state = []
-        (i, j) = action
-        for k in range(len(old_state)):
-            if k != i:
-                new_state.append(old_state[k])
-            else:
-                new_state.append(old_state[k] - j)
 
-        # g = Nim(new_state)
-        # possible_actions = g.possible_actions()
+        new_state = switch_state(old_state, action)
         new_action = self.choose_action(new_state, epsilon_greedy=False)
         if new_action != 0 and new_action != None:
             new_new_state = switch_state(new_state, new_action)
-            best_reward = self.max_reward(new_new_state)
+            game = Nim(new_new_state)
+            possible_actions = game.possible_actions()
+            if possible_actions == []:
+                best_reward = 0
+            else:
+                best_reward = None
+                for a in possible_actions:
+                    if (tuple(new_new_state), a) in self.Qtable:
+                        a_value = self.Qtable[(tuple(new_new_state), a)]
+                    else:
+                        a_value = 0
+                    if  best_reward == None or a_value > best_reward:
+                        best_reward = a_value
         else:
             best_reward = 0
 
-        new_q = old_q + self.alpha * ((reward + best_reward) - old_q)
+        # update q value according to bellman equation
+        new_q = old_q + self.alpha * ((reward + self.gamma * best_reward) - old_q)
         self.Qtable[(tuple(old_state), action)] = new_q
-
-
 
 
         
 
-# ============================================================================
+# ========================= TRAINING THE MODEL ===================================================
 
-# piles = generate_piles(10, 10) # todo qlearning mora sprejet začetno stanje al ne?
-piles = [5, 5, 5]
+# pri taki začetni postavitvi dobim slabše rezultate ampak to je logično, ker če je random prvi 
+# in se mu posreči da izbere ravno (1,2) (za kar je 1/3 možnosti) bo seveda zmagal
+# piles = [0,3,0]
+
+piles = [5,5,5] 
 
 def train_model(n, algorithm=Qlearning):
     """Training the model n times with algorithm (for e/xample Qlearning()). 
     We train the model with the same game all the time?"""
 
     for i in range(n):
-
         game = Nim(piles)
-        
         # last move made by either player
         moves = {0 : [None, None], 1 : [None, None]} # (state, action)
 
         play = True
-        while play: # game.winner == -1:
+        while play: 
             
-
             current_state = game.piles.copy()
-
             action = algorithm.choose_action(current_state)
 
-            # print((current_state, action))
+            moves[game.player][0] = current_state 
+            moves[game.player][1] = action 
 
-            if False: #action == 0:
-                print("tukej sm NIKOLI")
-                play = False
-            else:
-
-                moves[game.player][0] = current_state 
-                moves[game.player][1] = action 
-
-
-                game.make_move(action) # switches player 
-                new_state = game.piles.copy()
-
-                # print(new_state)
-
-                if game.winner != -1: # and moves[game.player][0] is not None: # if someone won update q values with rewards
-
+            game.make_move(action) # switches player 
+            if game.winner != -1:
+                if moves[game.player][0] is not None:
                     algorithm.update_model(moves[game.player][0], moves[game.player][1], 1)
                     algorithm.update_model(current_state, action, -1)
                     play = False
+                else: # meaning only 2 moves were made
+                    algorithm.update_model(current_state, action, -1)
+                    play = False
 
-                elif game.winner == -1: # and moves[game.player][0] is not None:
-
-                    # algorithm.update_model(current_state, action, 0) # no rewards yet
-                    if moves[game.player][0] is not None:
-                        algorithm.update_model(moves[game.player][0], moves[game.player][1], 0)
-                        
-                    # else:
-                    #     algorithm.update_model(current_state, action, 0)
-
+            elif game.winner == -1: 
+                if moves[game.player][0] is not None:
+                    algorithm.update_model(moves[game.player][0], moves[game.player][1], 0)
                 
 
-        # print(algorithm.Qtable)
-        # print(moves)
-    # print(i)
     return algorithm
 
-def play(algorithm=Qlearning(0.5, 0.1), human=None):
 
+# ===================== FUNCTIONS FOR PLAYING =======================================
+
+def play(algorithm=Qlearning(0.5, 0.1, 0.1), human=None):
     """Human is either 0 or 1"""
     if human == None:
-        human = random.randint(0, 1)
+        human = random.randint(0, 1) # randomly chose who if human is player 1 or 0
     game = Nim(piles)
 
     while game.winner == -1:
-        # print(game.piles)
-
-        legit = True
         possible_actions = game.possible_actions()
         print(game.piles)
         if game.player == human:
@@ -188,18 +151,14 @@ def play(algorithm=Qlearning(0.5, 0.1), human=None):
 
             action = (pile, count)
             if action not in possible_actions:
-                
-                raise Exception("Not a possible action!") # todo 
-                legit = False
+                raise Exception("Not a possible action!")  
+                return
         else:
             print("AI's turn!")
-            # possible_actions = game.possible_actions()
-            # print(possible_actions)
             state = game.piles.copy()
             action = algorithm.choose_action(state, epsilon_greedy=False)
 
-        if action != 0 and legit == True:
-            # print(action)
+        if action != 0:
             game.make_move(action)
         else:
             print(game.check_for_winner())
@@ -210,57 +169,47 @@ def play(algorithm=Qlearning(0.5, 0.1), human=None):
                 w = "HUMAN"
             else:
                 w = "AI"
-    # print(w)
-    return w
+    return "Winner is: " + w
 
-alg = Qlearning(0.5, 0.1)
+alg = Qlearning(0.5, 0.1, 0.1)
 train_model(10000, alg)
-# play(alg)
 
+# ============================== PLAYING WITH A RANDOM OPPONENT ===================================
 
-            
-# alg.Qtable
-# {((5, 5, 5), (0, 1)): 7e-323, ((4, 5, 5), (0, 1)): 6.4e-323, ((3, 5, 5), (0, 1)): 6e-323, ((2, 5, 5), (0, 1)): 5.4e-323, ((1, 5, 5), (0, 1)): 5e-323, ((0, 5, 5), (1, 1)): 4.4e-323, ((0, 4, 5), (1, 1)): 4e-323, ((0, 3, 5), (1, 1)): 3.5e-323, ((0, 2, 5), (1, 1)): 3e-323, ((0, 1, 5), (1, 1)): 2.5e-323, ((0, 0, 5), (2, 1)): 2e-323, ((0, 0, 4), (2, 1)): 1.5e-323, ((0, 0, 3), (2, 1)): 1e-323, ((0, 0, 1), (2, 1)): -1.0, ((0, 0, 2), (2, 1)): 5e-324}
-
-
-# todo če natreniraš na 10 stolpcev po 10 kovancev lahko potem poženeš na random igri (10, 10)
 
 def random_opponent(n, algorithm):
-
     """Play against random opponent n times and count wins"""
+    
     wins = {"ai" : 0, "random opponent" : 0}
     for i in range(n):
         opponent = random.randint(0, 1)
         game = Nim(piles)
 
         while game.winner == -1:
-            # print(game.piles)
-
-            legit = True
             possible_actions = game.possible_actions()
-            # print(game.piles)
             if game.player == opponent:
-
                 action = random.choice(possible_actions)
 
             else:
-
                 state = game.piles.copy()
                 action = algorithm.choose_action(state, epsilon_greedy=False)
 
-            if action != 0 and legit == True:
-                # print(action)
+            if action != 0:
                 game.make_move(action)
             else:
                 print(game.check_for_winner())
 
             if game.winner != -1:
-
                 if game.winner == opponent:
                     wins["random opponent"] += 1
                 else:
                     wins["ai"] += 1
-    # print(w)
     return wins
 
+print(random_opponent(100, alg))
+print(random_opponent(100, alg))
+print(random_opponent(100, alg))
+print(random_opponent(100, alg))
+print(random_opponent(100, alg))
+print(random_opponent(100, alg))
 print(random_opponent(100, alg))
